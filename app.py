b620017cb7d22ce075b52f479c9c7eed2af9e2a6
Luis Gómez <luisdanielgm19@gmail.com>
2024-01-13 00:35:23 -0400

    add all content

--- app.py
+++ app.py
@@ -1,47 +1,59 @@
-from flask import Flask, jsonify
-import requests
-from bs4 import BeautifulSoup
-
-app = Flask(__name__)
+# Función para realizar el scraping de una URL específica
+def scrape_inner_content(inner_url):
+    response = requests.get(inner_url)
+    inner_content = response.content
+    inner_soup = BeautifulSoup(inner_content, 'html.parser')
+    
+    # Busca el contenido dentro de la etiqueta <div> con la clase 'article-content'
+    inner_contenido = inner_soup.find('div', class_='article-content')
+    
+    # Extrae el texto del contenido
+    if inner_contenido:
+        return inner_contenido.get_text(strip=True)
+    else:
+        return None
 
 # Definición de la ruta para el scraping
 @app.route('/genbeta', methods=['GET'])
 def scrape_website():
     # Tu código de scraping
     url = "https://www.genbeta.com/tag/inteligencia-artificial"
-    respuesta = requests.get(url)
-    contenido = respuesta.content
-    soup = BeautifulSoup(contenido, 'html.parser')
+    response = requests.get(url)
+    content = response.content
+    soup = BeautifulSoup(content, 'html.parser')
     
     # Busca todos los elementos <article> con la clase 'recent-abstract abstract-article'
-    articulos = soup.find_all('article', class_='recent-abstract abstract-article')
+    articles = soup.find_all('article', class_='recent-abstract abstract-article')
 
     # Lista para almacenar los resultados
     scraped_data = []
 
     # Itera sobre los artículos
-    for articulo in articulos:
+    for article in articles:
         # Extrae la URL de la imagen del primer elemento <img>
-        img_url = articulo.find('img')['src']
+        img_url = article.find('img')['src']
         
         # Extrae el título del artículo del elemento <h2> con la clase 'abstract-title'
-        titulo = articulo.find('h2', class_='abstract-title').find('a').get_text()
+        title = article.find('h2', class_='abstract-title').find('a').get_text()
 
-        # Extrae el contenido del artículo del elemento <div> con la clase 'abstract-excerpt'
-        contenido = articulo.find('div', class_='abstract-excerpt').find('p').get_text()
+        # Extrae la URL del enlace dentro de la etiqueta <a> en el div con la clase 'abstract-excerpt'
+        link_url = article.find('div', class_='abstract-excerpt').find('a')['href']
+
+        # Realiza el scraping de la URL interna para obtener el contenido adicional
+        inner_content = scrape_inner_content(link_url)
 
         # Crea un diccionario con la información y agrega a la lista
-        articulo_info = {
-            'titulo': titulo,
+        article_info = {
+            'title': title,
             'img_url': img_url,
-            'contenido': contenido
+            'link_url': link_url,
+            'inner_content': inner_content
         }
-        scraped_data.append(articulo_info)
+        scraped_data.append(article_info)
 
     # Devuelve los resultados como JSON
     return jsonify({'data': scraped_data})
 
+# Ejecuta la aplicación si se ejecuta este script
 if __name__ == '__main__':
-    # Puedes cambiar el puerto aquí
-    # puerto = 8080
     app.run(debug=True)
