913800e482f7020d8e054f9c24f4e794ef70f539
Luis Gómez <luisdanielgm19@gmail.com>
2024-01-17 19:32:22 -0400

    requirements flask_cors

--- app.py
+++ app.py
@@ -5,19 +5,15 @@ from feeds.genbeta.scraping import scrape_genbeta
 app = Flask(__name__)
 CORS(app)
 
-
-
 @app.route('/', methods=['GET'])
 def get_all_news():
     scraped_data = scrape_genbeta()
     return jsonify({'data': scraped_data})
 
-
 @app.route('/genbeta', methods=['GET'])
 def get_genbeta_news():
     scraped_data = scrape_genbeta()
     return jsonify({'data': scraped_data})
-
 
 # Ejecuta la aplicación si se ejecuta este script
 if __name__ == '__main__':
